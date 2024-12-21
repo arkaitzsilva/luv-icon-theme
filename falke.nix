61a47df4b0c26e3e7a23ab7897de92c90d566251
arkaitzsilva <arkaitz.develop@gmail.com>
2024-12-21 18:38:16 +0100

    nix config files update

--- falke.nix
+++ falke.nix
@@ -1,26 +1,22 @@
 {
-  description = "Luv Icon Theme";
+  description = "Luv-Dark Icon Theme";
 
   outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux.luvIconTheme = let
+    packages.x86_64-linux.luv-icon-theme = let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in pkgs.stdenv.mkDerivation {
-      name = "luv-icon-theme";
+      name = "luv-dark-icon-theme";
       src = ./.;
       installPhase = ''
-        mkdir -p $out/share/icons/${pname}
-        cp -r Luv-Dark/* $out/share/icons/${pname}/
+        mkdir -p $out/share/icons
+        cp -r Luv-Dark $out/share/icons/
       '';
       meta = with pkgs.lib; {
-        description = "Un tema de iconos Luv para Linux";
+        description = "luv-icon-theme";
         homepage = "https://github.com/arkaitzsilva/luv-icon-theme";
         license = licenses.gpl3Plus;
         maintainers = [ maintainers.arkaitzsilva ];
       };
-    };
-
-    nixosModules.luvIconTheme = { config, pkgs, ... }: {
-      environment.systemPackages = [ pkgs.luvIconTheme ];
     };
   };
 }
