166f25db4c614d9e76cc19c4940a14956d7d0b5c
arkaitzsilva <arkaitz.develop@gmail.com>
2025-01-05 20:36:36 +0100

    changes

--- flake.nix
+++ flake.nix
@@ -1,41 +1,25 @@
-{
-  lib,
-  stdenvNoCC,
-  fetchFromGitHub,
-  gtk3,
-  xdg-utils,
-}:
-stdenvNoCC.mkDerivation rec {
+{ lib, stdenv, fetchFromGitHub, installShell }:
+
+stdenv.mkDerivation rec {
   pname = "luv-icon-theme";
-  version = "0.1";
+  version = "1.0";
 
   src = fetchFromGitHub {
     owner = "arkaitzsilva";
     repo = "luv-icon-theme";
-    rev = "${version}";
-    hash = "lib.fakeSha256";
+    rev = "main";
+    sha256 = lib.fakeSha256;
   };
 
-  nativeBuildInputs = [
-    gtk3
-    xdg-utils
-  ];
-
   installPhase = ''
-    runHook preInstall
-
-    install -d $out/share/icons/Luv-Dark
-    cp -r . $out/share/icons/Luv-Dark
-    gtk-update-icon-cache -f -t $out/share/icons/Luv-Dark && xdg-desktop-menu forceupdate
-
-    runHook postInstall
+    mkdir -p $out/share/icons/${pname}
+    cp -r Luv-Dark/* $out/share/icons/${pname}/
   '';
 
   meta = with lib; {
     description = "Luv Icon Theme";
     homepage = "https://github.com/arkaitzsilva/luv-icon-theme";
     license = with licenses; [ gpl3Only ];
-    platforms = platforms.linux;
     maintainers = with maintainers; [ arkaitzsilva ];
   };
 }
